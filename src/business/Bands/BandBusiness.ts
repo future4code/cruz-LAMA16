6492e272449351df1f4f0cc9e77861c604de8de9
AngeloVSO <angelo_odwyer@hotmail.com>
2021-06-25 18:26:58 -0300

    Endpoint de detalhes das bandas

--- src/business/Bands/BandBusiness.ts
+++ src/business/Bands/BandBusiness.ts
@@ -6,7 +6,7 @@ import { idGenerator } from "../../services/IdGenerator";
 import CustomError from "../erros/CustomError";
 
 class BandBusiness {
-  public createBand = async (token: any, bandInput: BandDTO) => {
+  public createBand = async (token: any, bandInput: BandDTO): Promise<void> => {
     try {
       if (!bandInput.name || !bandInput.music_genre || !bandInput.responsible) {
         throw new CustomError(400, "All fields are required!");
@@ -26,19 +26,51 @@ class BandBusiness {
         throw new CustomError(401, "Only 'ADMIN' can register bands");
       }
 
-      const [bandAlreadyExists] = await BandDatabase.selectGeneric("name", {name: bandInput.name});
-      console.log("verific se retorna banda", bandAlreadyExists)
+      const [bandAlreadyExists] = await BandDatabase.selectGeneric("name", {
+        name: bandInput.name,
+      });
+      console.log("verific se retorna banda", bandAlreadyExists);
 
       if (bandAlreadyExists) {
         throw new CustomError(422, "This band already registred!");
       }
 
-      const band:Bands = {
-          ...bandInput,
-          id: idGenerator()
+      const band: Bands = {
+        id: idGenerator(),
+        name: bandInput.name.toLowerCase(),
+        music_genre: bandInput.music_genre.toLowerCase(),
+        responsible: bandInput.responsible.toLowerCase()
+      };
+
+      await BandDatabase.insertGeneric(band);
+    } catch (error) {
+      throw new CustomError(error.statusCode, error.message);
+    }
+  };
+
+  public getBandByIdOrName = async (idOrName: string): Promise<Bands> => {
+    try {
+      const [bandId]: Bands[] = await BandDatabase.selectGeneric("*", {
+        id: idOrName,
+      });
+
+      const [bandName]: Bands[] = await BandDatabase.selectGeneric("*", {
+        name: idOrName,
+      });
+
+      let band = null;
+
+      if (bandId) {
+        band = bandId;
+      } else if (bandName) {
+        band = bandName;
       }
 
-      await BandDatabase.insertGeneric(band)
+      if (!bandId && !bandName) {
+        throw new CustomError(404, "Band not found");
+      }
+
+      return band as Bands;
     } catch (error) {
       throw new CustomError(error.statusCode, error.message);
     }
