0f0efabe537f44caed9e625ef99953db239b49b7
samenc99 <mateus.enc@gmail.com>
2021-06-28 06:42:25 -0300

    adicionando cors

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import bandRouter from './routes/bandRoutes';
 import userRouter from "./routes/userRouter";
 import showRouter from "./routes/showRouter";
+import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use('/user', userRouter)
 app.use('/band', bandRouter)
