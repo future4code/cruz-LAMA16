3206fae0aca4fbc81444822043ca7c791f555cac
samenc99 <mateus.enc@gmail.com>
2021-06-28 05:05:18 -0300

    buyTicket

--- src/routes/showRouter.ts
+++ src/routes/showRouter.ts
@@ -1,6 +1,8 @@
 import {Router} from "express";
 import createShow from "../controllers/Shows/createShow";
 import getShows from "../controllers/Shows/getShows";
+import createTicket from "../controllers/Tickets/createTicket";
+import buyTicket from "../controllers/Tickets/buyTicket";
 
 
 const showRouter = Router()
@@ -8,3 +10,6 @@ export default showRouter
 
 showRouter.post('/create', createShow)
 showRouter.get('/:weekDay', getShows)
+
+showRouter.post('/ticket', createTicket)
+showRouter.put('/ticket', buyTicket)
