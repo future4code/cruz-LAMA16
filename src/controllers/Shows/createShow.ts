77f61b2923853b37cc9b38b91a629b5a797fabad
samenc99 <mateus.enc@gmail.com>
2021-06-28 06:04:47 -0300

    ultimos ajustes

--- src/controllers/Shows/createShow.ts
+++ src/controllers/Shows/createShow.ts
@@ -3,7 +3,7 @@ import showBusiness from "../../business/ShowBusiness";
 
 const  createShow=async (req: Request, res: Response): Promise<any> => {
   try {
-    await showBusiness.addShow(req.body)
+    await showBusiness.addShow(req.body, req.headers.authorization)
     res.status(200).send({message:'Created!'})
   } catch (err) {
     res.status(err.statusCode || 400).send({message: err.message || err.sqlMessage})
